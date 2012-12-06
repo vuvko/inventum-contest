fcbc093d5be30eaf33a8b0fda35c3a3759278f23
Andrei Shadrikov <6129086+vuvko@users.noreply.github.com>
2012-12-06 22:39:09 +0400

    ???????? ???? ??????????. ?????????? ????? ??????????? ????????.

--- SaveResult.m
+++ SaveResult.m
@@ -1,8 +1,8 @@
 clear
 load contestinventumdata.mat test9 test10
-test9 = DjBenchmark1(test9);
+test9 = SdvReclaim(test9);
 test9 = test9(:,end); % для экономии памяти допускается присылка только целевого вектора
-test10 = DjBenchmark1(test10);
+test10 = SdvReclaim(test10);
 test10 = test10(:,end); % для экономии памяти допускается присылка только целевого вектора
 name = 'Шадриков Андрей';
 save solution.mat name test9 test10
